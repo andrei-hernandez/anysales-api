4ebdb8effdb8232023b3e7c6bbeaf294affb15b1
Andrei Hernandez <andiuchihahaha@gmai.com>
2024-01-05 13:40:26 -0600

    added some quality code tools

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,9 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { NestFactory } from "@nestjs/core"
+import { AppModule } from "./app.module"
 
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+async function bootstrap(): Promise<void> {
+  const app = await NestFactory.create(AppModule)
+  const PORT = process.env.PORT || 3000
+  await app.listen(PORT)
 }
-bootstrap();
+bootstrap().then(() => null)
